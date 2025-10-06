0779ac06a5af89c5ffaa61d44ddc04468201a982
Seth Woolley <seth.w.public@proton.me>
2025-10-06 10:07:21 +0100

    nvim: Switched functionality to gitsigns
    gitsigns has a lot of very handy hunk staging functionality since last I
    checked

--- nvim/lua/plugins/vcs.lua
+++ nvim/lua/plugins/vcs.lua
@@ -5,25 +5,58 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     opts = {
-        signs = {
+      signs = {
         add = { text = '+' },
         change = { text = '~' },
         delete = { text = '_' },
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      current_line_blame_formatter = '      <committer> - <sha> - <summary>',
+
       on_attach = function(bufnr)
-        vim.keymap.set('n', '[c', require('gitsigns').prev_hunk, { buffer = bufnr })
-        vim.keymap.set('n', ']c', require('gitsigns').next_hunk, { buffer = bufnr })
+        local gitsigns = require('gitsigns')
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        map(
+          'n', '[c', function() gitsigns.nav_hunk('prev') end,
+          { desc = 'prev [c]hange', buffer = bufnr }
+        )
+        map(
+          'n', ']c', function() gitsigns.nav_hunk('next') end,
+          { desc = 'next [c]hange', buffer = bufnr }
+        )
+        map(
+          'n', ' b', function() gitsigns.toggle_current_line_blame() end,
+          { desc = 'Git [b]lame line' }
+        )
+
+        map(
+          'n', ' gh', function() gitsigns.stage_hunk() end,
+          { desc = '[g]it [h]unk stage' }
+        )
+        map(
+          'v', ' gh', function() gitsigns.stage_hunk() end,
+          { desc = '[g]it [h]unk stage' }
+        )
+
+        map(
+          'n', ' gd', function() gitsigns.preview_hunk_inline() end,
+          { desc = '[g]it [d]iff' }
+        )
       end,
-    }
+    },
   },
 
-  -- inline git-blame and blame commit copying
+  -- blame commit copying
   {
     'f-person/git-blame.nvim',
     keys = {
-      {' b', '<cmd>GitBlameToggle<CR>', desc = 'Git [b]lame'},
       {' bc', '<cmd>GitBlameCopySHA<CR>', desc = 'Git [b]lame [c]opy commit ID'},
     },
     opts = {
